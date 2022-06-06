8d35735d7c75e03b935bb7a05707abfe82da85a3
diegocastilloupgrade <diego.castillo@bootcamp-upgrade.com>
2022-06-06 14:05:39 +0200

    subimos todo

--- comunidadesporid.js
+++ comunidadesporid.js
@@ -1,4 +1,5 @@
 const apiUrl = 'http://localhost:8000/ccaa';
+//const ciudadesNameAsturias = 'http://localhost:8000/ccaa/name/Asturias';
 
 const getComunidadesPorId = async () => {
   const comunidadData = await fetch(apiUrl);
@@ -27,5 +28,34 @@ const getComunidadesPorId = async () => {
     ccaaContainer$$.appendChild(ccaaCapital$$);
   }
 };
-
 getComunidadesPorId();
+/*
+const getComunidadesPorId = async () => {
+  const comunidadData = await fetch(apiUrl);
+  const comunidadDataJSON = await comunidadData.json();
+  const datosComunidades = comunidadDataJSON.comunidades;
+
+  // const miInput$$ = document.querySelector('.miInput');
+
+  for (let i = 0; i < datosComunidades.length; i++) {
+    const myComunidadData$$ = document.querySelector('.comunidades');
+    const ccaaContainer$$ = document.createElement('div');
+    ccaaContainer$$.className = 'divPadre';
+    const ccaaName$$ = document.createElement('h1');
+    const ccaaPopulation$$ = document.createElement('p');
+    const ccaaCapital$$ = document.createElement('p');
+    const ccaaBandera$$ = document.createElement('img');
+    ccaaBandera$$.setAttribute('src', datosComunidades[i].bandera);
+    ccaaName$$.innerText = datosComunidades[i].name;
+    ccaaPopulation$$.innerText = datosComunidades[i].poblacion;
+    ccaaCapital$$.innerText = datosComunidades[i].capital;
+    myComunidadData$$.appendChild(ccaaContainer$$);
+
+    ccaaContainer$$.appendChild(ccaaBandera$$);
+    ccaaContainer$$.appendChild(ccaaName$$);
+    ccaaContainer$$.appendChild(ccaaPopulation$$);
+    ccaaContainer$$.appendChild(ccaaCapital$$);
+  }
+};
+*/
+//getComunidadesPorNamexxx();
